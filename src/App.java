7aeecf0c430ce475e01e0dc54405236607ced0c1
Elias D. Heinzen <eliasheinzen@gmail.com>
2023-04-07 18:45:48 -0300

    utilizando api da nasa

--- src/App.java
+++ src/App.java
@@ -12,7 +12,10 @@ public class App {
     public static void main(String[] args) throws Exception {
         // fazer uma conexão HTTP e buscar os top 250 filmes
         // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json"; // usando endereço alternativo
+        // String url =
+        // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        // // usando endereço alternativo
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
         URI endereco = URI.create(url);
         var client = HttpClient.newHttpClient();
         var request = HttpRequest.newBuilder(endereco).GET().build();
@@ -25,9 +28,12 @@ public class App {
 
         // exibir e manipular os dados
         var geradora = new GeradoraDeFigurinhas();
-        for (Map<String,String> filme : listaDeFilmes) {
+        for (Map<String, String> filme : listaDeFilmes) {
 
-            String urlImagem = filme.get("image");
+            String urlImagem =
+                // filme.get("image");
+                filme.get("url");
+
             String titulo = filme.get("title");
 
             InputStream inputStream = new URL(urlImagem).openStream();
