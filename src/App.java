28c98bdbc306555c709e20a79a3f6b3d36100d24
Elias D. Heinzen <eliasheinzen@gmail.com>
2023-04-09 22:01:57 -0300

    refatorando o código

--- src/App.java
+++ src/App.java
@@ -1,10 +1,5 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
@@ -12,29 +7,29 @@ public class App {
     public static void main(String[] args) throws Exception {
         // fazer uma conexão HTTP e buscar os top 250 filmes
         // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
-        // String url =
-        // "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         // // usando endereço alternativo
         String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
-        URI endereco = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(endereco).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+
+        var http = new ClienteHttp();
+        String json = http.buscaDados(url);
 
         // extrair só os dados que interessam (título, poster, classificação)
         var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        List<Map<String, String>> listaDeConteudos = parser.parse(json);
 
         // exibir e manipular os dados
         var geradora = new GeradoraDeFigurinhas();
-        for (Map<String, String> filme : listaDeFilmes) {
+        
+        for (int i = 0; i < 3; i++) {
+
+            Map<String, String> conteudo = listaDeConteudos.get(i);
 
             String urlImagem =
-                // filme.get("image");
-                filme.get("url");
+                // conteudo.get("image");
+                conteudo.get("url");
 
-            String titulo = filme.get("title");
+            String titulo = conteudo.get("title");
 
             InputStream inputStream = new URL(urlImagem).openStream();
             String nomeArquivo = "saida/" + titulo + ".png";
